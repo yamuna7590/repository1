da0c5158bd0a78d406022634ac8464d62719c043
yamuna7590 <37892416+yamuna7590@users.noreply.github.com>
2018-03-29 21:03:33 +0530

    Update Consumer.java

--- Consumer.java
+++ Consumer.java
@@ -19,7 +19,7 @@ package sample.activemq;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Consumer {
 
 	@JmsListener(destination = "sample.queue")
